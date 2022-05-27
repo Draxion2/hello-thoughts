03a089c0711f903a0cf0d5e907d40298658522ba
= <holdanmcmullen@gmail.com>
2022-05-27 15:36:54 -0600

    User model

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: "Username is required!",
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+})
+
+// get total count of friends and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return "Friends: " + this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+// export the User model
+module.exports = User;
